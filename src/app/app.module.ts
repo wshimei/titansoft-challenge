cc47a1db3ee0d223eb7f67e9eafe3367199cd86d
Shimei Wong <wshimei@gmail.com>
2018-07-05 15:05:38 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
