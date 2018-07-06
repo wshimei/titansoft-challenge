1e5c48193fc9535e863836155d6551ad34b0eba0
Shi Mei Wong <wshimei@mgmail.com>
2018-07-06 13:15:42 +0800

    product list ok

---
+++ src/app/product.ts
@@ -0,0 +1,7 @@
+export class Product {
+    id: number;
+    name: string;
+    imageURL: string;
+    price: number;
+    shipping: string;
+}
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+
+  product: Product = {
+    id: 1,
+    name: 'Mini X',
+    imageURL: '//cdn.shopify.com/s/files/1/0325/7933/products/boosted-stealth_small.jpg?653946253001957546',
+    price: 999,
+    shipping: 'NA / Ships in about 4 weeks'
+  };
 
   constructor() { }
 
